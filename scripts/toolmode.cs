c55c3ff750715172f0d91ba56525f87a402b2177
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2017-11-25 07:04:52 -0800

    Improve formatting of /BTTHelp
    More readible and more concise.

--- scripts/toolmode.cs
+++ scripts/toolmode.cs
@@ -66,42 +66,34 @@ function GameConnection::BTT_setMode(%this, %mode, %noTextUpdate) {
 		%this.BTT_updateImage();
 }
 
-function servercmdBTTHelp(%this, %section) {
-	%this.chatMessage("\c1==== The Better Trench Tool Guide ====");
+function serverCmdBTTHelp(%this, %section) {
+	%bullet = "  <font:impact:17>\c9*  <font:palatino linotype:25>";
+	%this.chatMessage(" ");
+	%this.chatMessage("<rmargin:400><just:center><font:Impact:40><shadow:2:2>\c6Better Trench Tool Help");
+	%this.chatMessage("<rmargin:400><just:center><font:Impact:20><shadow:2:2>\c6---------------------------------------------------------------------");
 	if (%section == 1) {
-		%this.chatMessage("\c1---- Description ----");
-		%this.chatMessage("\c6The \c4Better Trench Tool \c6is an tool which combines the use of");
-		%this.chatMessage("\c6both trench tools, the \c4Trench Shovel \c6and the \c4Trench Dirt\c6,");
-		%this.chatMessage("\c6into one, while also providing a few additional features.");
-		%this.chatMessage("\c6One feature the \c4Better Trench Tool \c6provides is a ghost brick.");
-		%this.chatMessage("\c6This ghost brick is much like the one provided by the standard build");
-		%this.chatMessage("\c6tool, in that it places a transparent block where you are looking so");
-		%this.chatMessage("\c6you know where you are going to dig/place dirt.");
-		%this.chatMessage("\c6Another feature the \c4Better Trench Tool \c6provides is the ability");
-		%this.chatMessage("\c6to adjust the amount of dirt you can dig/place at once. Instead of");
-		%this.chatMessage("\c6just digging/placing one brick at a time, you choose to dig/place");
-		%this.chatMessage("\c6multiple bricks in a single click. This change in your shovel/placer");
-		%this.chatMessage("\c6size will be seen in your ghost brick.");
-		%this.chatMessage("\c6Some information--like the size of your shovel/place and how much dirt");
-		%this.chatMessage("\c6you have--will be present at the bottom of your screen when the");
-		%this.chatMessage("\c4Better Trench Tool \c6is equipped.");
-		%this.chatMessage("\c6All of this should become more clear when you actually have the");
-		%this.chatMessage("\c4Better Trench Tool \c6equipped.");
-		%this.chatMessage("\c3(Use Page Up and Page Down to see everything)");
+		%this.chatMessage("<rmargin:400><just:center><font:Impact:30>\c6Description:<br>");
+		%this.chatMessage("<rmargin:1000><just:left>");
+		%this.chatMessage(%bullet SPC "\c6The \c4Better Trench Tool \c6works as a dirt placer and shovel.");
+		%this.chatMessage(%bullet SPC "\c6It also allows you to adjust how much dirt you can dig and place at a time.");
 	}
 	else if (%section == 2) {
-		%this.chatMessage("\c1---- Controls ----");
-		%this.chatMessage("\c6Use the <Light Key> \c6to switch between modes.");
-		%this.chatMessage("\c3Shovel Mode \c6is for digging trench dirt.");
-		%this.chatMessage("\c3Placer Mode \c6is for placing trench dirt.");
-		%this.chatMessage("\c6Use <Shift Brick Up> \c6to increase the amount of dirt you place/dig.");
-		%this.chatMessage("\c6Use <Shift Brick Down> \c6to decrease the amount of dirt you place/dig.");
-		%this.chatMessage("\c6Change your paint color to change the color of the dirt you place.");
+		%this.chatMessage("<rmargin:400><just:center><font:Impact:30>\c6Controls:<br>");
+		%this.chatMessage("<rmargin:1000><just:left>");
+		%this.chatMessage(%bullet SPC "\c6Use the <Light Key> \c6to switch between modes.");
+		%this.chatMessage(%bullet SPC "\c3Shovel Mode \c6is for digging trench dirt.");
+		%this.chatMessage(%bullet SPC "\c3Placer Mode \c6is for placing trench dirt.");
+		%this.chatMessage(%bullet SPC "\c6Use <Shift Brick Up> \c6to increase the amount of dirt you can place and dig.");
+		%this.chatMessage(%bullet SPC "\c6Use <Shift Brick Down> \c6to decrease the amount of dirt you can place and dig.");
+		%this.chatMessage(%bullet SPC "\c6Use <Super Shift> \c6to quickly change between the smallest and biggest cube size.");
+		%this.chatMessage(%bullet SPC "\c6Change your paint color to change the color of the dirt you place.");
+		%this.chatMessage("      \c6(if the host has enabled this option)");
 	}
 	else {
-		%this.chatMessage("\c1---- Help Sections ----");
-		%this.chatMessage("\c61 - \c3Description");
-		%this.chatMessage("\c62 - \c3Controls");
+		%this.chatMessage("<rmargin:400><just:center><font:Impact:30>\c6Help Sections:<br>");
+		%this.chatMessage("<rmargin:1000><just:left>");
+		%this.chatMessage("  \c61 - \c3Description");
+		%this.chatMessage("  \c62 - \c3Controls");
 	}
 }
 
