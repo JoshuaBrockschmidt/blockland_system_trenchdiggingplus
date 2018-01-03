e2199dd466d74cb5c564d31a21538a4cb976c3a4
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2018-01-03 01:45:14 -0800

    Add bricks
    Also remove compatibility file script. Compatibilty will be more implicit
    than explicit from here on.

--- server.cs
+++ server.cs
@@ -1,11 +1,16 @@
-if(forceRequiredAddOn("Player_No_Jet") == $Error::AddOn_NotFound) {
+if (forceRequiredAddOn("Player_No_Jet") == $Error::AddOn_NotFound) {
 	error("Player_No_Jet not found! Trench Digging Plus will not be loaded");
+	return;
+}
+
+if (forceRequiredAddOn("Brick_Large_Cubes") == $Error::AddOn_NotFound) {
+	error("Brick_Large_Cubes not found! Trench Digging Plus will not be loaded");
 	return;
 }
 
 $TDP::filePath = filePath($Con::File) @ "/";
 $TDP::serverPath = $TDP::filePath @ "server/";
 
-exec($TDP::serverPath @ "core/compat.cs");
+exec($TDP::serverPath @ "bricks/datablocks.cs");
 exec($TDP::serverPath @ "players/datablocks.cs");
 exec($TDP::serverPath @ "tools/trenchtool/server.cs");
---
+++ server/bricks/datablocks.cs
@@ -0,0 +1,88 @@
+datablock fxDTSBrickData(brick2xCubeDirtData)
+{
+	brickFile = "./2x Cube Dirt.blb";
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "2x Cube Dirt";
+	iconName = $TDP::serverPath @ "bricks/2x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick4xCubeDirtData : brick4xCubeData)
+{
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "4x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick8xCubeDirtData : brick8xCubeData)
+{
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "8x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick16xCubeDirtData : brick16xCubeData)
+{
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "16x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick32xCubeDirtData : brick32xCubeData)
+{
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "32x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick64xCubeDirtData : brick64xCubeData)
+{
+	category = "Dirt";
+	subCategory = "Cube";
+	uiName = "64x Cube Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick1x1DirtData : brick1x1Data)
+{
+	category = "Dirt";
+	subCategory = "Brick";
+	uiName = "1x1 Dirt";
+	isTrenchDirt = true;
+};
+datablock fxDTSBrickData(brick2x2DirtData : brick2x2Data)
+{
+	category = "Dirt";
+	subCategory = "Brick";
+	uiName = "2x2 Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick4x4DirtData : brick4x4Data)
+{
+	category = "Dirt";
+	subCategory = "Brick";
+	uiName = "4x4 Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick8x8DirtData : brick8x8Data)
+{
+	category = "Dirt";
+	subCategory = "Brick";
+	uiName = "8x8 Dirt";
+	isTrenchDirt = true;
+};
+
+datablock fxDTSBrickData(brick8x16DirtData : brick8x16Data)
+{
+	category = "Dirt";
+	subCategory = "Brick";
+	uiName = "8x16 Dirt";
+	isTrenchDirt = true;
+};
