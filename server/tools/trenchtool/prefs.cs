34ab2efbf07c94b0e575a958f2f0cb80891bd695
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2018-02-01 21:58:54 -0800

    Convert tool color settings to a dropdown menu

--- server/tools/trenchtool/prefs.cs
+++ server/tools/trenchtool/prefs.cs
@@ -15,28 +15,32 @@ function TRT_updateCubeSizes() {
 	}
 }
 
-function TRT_updateColorDefault() {
-	// Default dirt color overrides players' choice of dirt color
-	if ($TRT::colorIsDefault)
-		// TODO: BLG does not yet express manual variable updates in the Glass Server Preferences GUI
+function TRT_updateColorPref(%val) {
+	switch (%val) {
+	case 1: // Actual dirt
 		$TRT::canChooseColor = false;
+		$TRT::colorIsDefault = false;
+
+	case 2: // Default
+		$TRT::canChooseColor = false;
+		$TRT::colorIsDefault = true;
+
+	default: // Paint color
+		$TRT::canChooseColor = true;
+		$TRT::colorIsDefault = false;
+	}
 }
 
 if (isObject(Glass) && Glass.serverLoaded) {
 	registerPref("Trench Digging Plus", "Trench Tool", "Max Cube Size", "dropdown", "TRT::maxCubeSize", "System_TrenchDiggingPlus", 4, "1 1 2 2 3 3 4 4 5 5 6 6", "TRT_updateCubeSizes", 0, 0, 0);
 	registerPref("Trench Digging Plus", "Trench Tool", "Range", "dropdown", "TRT::toolRange", "System_TrenchDiggingPlus", 10, "5 5 10 10 15 15 20 20", "", 0, 0, 0);
 	registerPref("Trench Digging Plus", "Trench Tool", "Delay Multiplier (seconds)", "num", "TRT::delayMult", "System_TrenchDiggingPlus", 1, "0 5 1", "", 0, 0, 0);
-	registerPref("Trench Digging Plus", "Trench Tool", "Players Choose Dirt Color", "bool", "TRT::canChooseColor", "System_TrenchDiggingPlus", true, "", "", 0, 0, 0);
-	registerPref("Trench Digging Plus", "Trench Tool", "Use Default Dirt Color", "bool", "TRT::colorIsDefault", "System_TrenchDiggingPlus", false, "", "TRT_updateColorDefault", 0, 0, 0);
+	registerPref("Trench Digging Plus", "Trench Tool", "Color Setting", "dropdown", "TRT::colorType", "System_TrenchDiggingPlus", 0, "Paint_Color 0 Actual_Dirt_Color 1 Default_Dirt_Color 2", "TRT_updateColorPref", 0, 0, 0);
 }
 
 // Maximum size of cubes for placing and digging dirt
 if ($TRT::maxCubeSize $= "")
 	$TRT::maxCubeSize = 4;
-	
-// Whether or not players can choose the color of their dirt
-if ($TRT::canChooseColor $= "")
-	$TRT::canChooseColor = true;
 
 // Maximum range of tool (both for digging and placing dirt)
 if ($TRT::toolRange $= "")
@@ -46,11 +50,9 @@ if ($TRT::toolRange $= "")
 if ($TRT::delayMult $= "")
 	$TRT::delayMult = 1;
 
-// Whether or not players can choose their own color
-if ($TRT::canChooseColor $= "") {
+// Whether players can choose the color of their dirt
+if ($TRT::canChooseColor $= "")
 	$TRT::canChooseColor = true;
-	TRT_updateColorDefault();
-}
 
 // Whether or not to use default color
 if ($TRT::colorIsDefault $= "")
